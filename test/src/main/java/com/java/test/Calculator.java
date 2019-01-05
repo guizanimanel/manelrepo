a801a2f111782fc35cba36ec347337ce6e42227e
Manel GUIZANI <manel.guizani@sofrecom.com>
2019-01-05 12:22:20 +0100

    Changement branchB 2

--- test/src/main/java/com/java/test/Calculator.java
+++ test/src/main/java/com/java/test/Calculator.java
@@ -3,7 +3,9 @@ package com.java.test;
 public interface Calculator {
 
 	// Changement branchA
-	// Changement branchB
+	// Changement branchB	1
+	// Changement branchB  2
+
 	int multiply(int a, int b);
 	int divide(int a, int b);
 	int add(int a, int b);
